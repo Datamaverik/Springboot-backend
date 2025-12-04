9f17ad809445834f0fc5426d9f07a9388cf1a12a
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-04 20:34:56 +0530

    feat: make Notification manager

--- src/main/java/com/datamaverik/store/NotificationManager.java
+++ src/main/java/com/datamaverik/store/NotificationManager.java
@@ -1,16 +1,21 @@
 package com.datamaverik.store;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationManager {
     private final NotificationService notificationService;
 
-    NotificationManager(NotificationService notificationService) {
+    NotificationManager(@Qualifier("email") NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
     public void sendNotification(String message) {
+        if(notificationService == null) {
+            System.out.println("notificationService is null");
+            return;
+        }
         notificationService.sendNotification(message);
     }
 }
