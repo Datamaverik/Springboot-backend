bf7f16fb4ed33e9afe1abb1172d9ffbbdb48276a
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-16 00:55:54 +0530

    feat: add dto

--- src/main/java/com/datamaverik/store/controllers/UserController.java
+++ src/main/java/com/datamaverik/store/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.datamaverik.store.controllers;
 
+import com.datamaverik.store.dtos.UserDto;
 import com.datamaverik.store.entities.User;
 import com.datamaverik.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,12 +19,21 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user ->
+                    new UserDto(user.getId(), user.getName(), user.getEmail())
+                )
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(new UserDto(user.getId(), user.getName(), user.getEmail()));
     }
 }
