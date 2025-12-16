f0cfb920d3201693945f4c76228999ecf64dd56f
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-17 00:03:29 +0530

    feat: add business rules valdiator

--- src/main/java/com/datamaverik/store/controllers/UserController.java
+++ src/main/java/com/datamaverik/store/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -47,9 +48,14 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
+    public ResponseEntity<?> registerUser(
             @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder) {
+        if(userRepository.existsByEmail(request.getEmail()))
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email already registered")
+            );
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
