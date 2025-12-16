a76e80b7433f974ec22889aec9cc40be58bd5cef
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-16 23:50:05 +0530

    feat: make the loca exception handler global for all contorllers

--- src/main/java/com/datamaverik/store/controllers/UserController.java
+++ src/main/java/com/datamaverik/store/controllers/UserController.java
@@ -11,13 +11,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -97,17 +94,5 @@ public class UserController {
         userRepository.save(user);
 
         return  ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(
-            MethodArgumentNotValidException ex
-    ) {
-        var errors = new HashMap<String, String>();
-        ex.getBindingResult().getFieldErrors().forEach((error) -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-
-        return ResponseEntity.badRequest().body(errors);
     }
 }
