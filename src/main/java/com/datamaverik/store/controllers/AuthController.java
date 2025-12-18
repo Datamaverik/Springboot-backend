ce31dd74f7e340ce203a2a2e1e992be4f4872c69
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-18 15:48:13 +0530

    feat: add spring security authenticator

--- src/main/java/com/datamaverik/store/controllers/AuthController.java
+++ src/main/java/com/datamaverik/store/controllers/AuthController.java
@@ -8,11 +8,11 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
@@ -24,16 +24,18 @@ public class AuthController {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(
             @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ) {
-        if(userRepository.existsByEmail(request.getEmail()))
+        if(userRepository.existsByEmail(request.getEmail())) {
             return ResponseEntity.badRequest().body(
                     Map.of("email", "Email already registered")
             );
+            }
 
         var user = userMapper.toEntity(request);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -47,16 +49,18 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@Valid @RequestBody LoginUserRequest request) {
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if(
-                user == null ||
-                        !passwordEncoder.matches(request.getPassword(), user.getPassword())
-        ) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    Map.of("error", "Invalid email or password")
-            );
-        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
